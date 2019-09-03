43556cf0e250dbf6ac629ff07f55dfcc425fa104
HENRY GARCIA OSPINA <henrygarciaospina@gmail.com>
2019-09-03 17:54:13 -0500

    Add contrats

--- MyLeasing.Web/Helpers/CombosHelper.cs
+++ MyLeasing.Web/Helpers/CombosHelper.cs
@@ -14,6 +14,26 @@ namespace MyLeasing.Web.Helpers
         {
             _dataContext = dataContext;
         }
+
+        public IEnumerable<SelectListItem> GetComboLessees()
+        {
+            var list = _dataContext.Lessees.Select(l => new SelectListItem
+            {
+                Text = l.User.FullNameWithDocument,
+                Value = $"{l.Id}"
+            })
+                .OrderBy(l => l.Text)
+                .ToList();
+
+            list.Insert(0, new SelectListItem
+            {
+                Text = "(Select a lessee...)",
+                Value = "0"
+            });
+
+            return list;
+        }
+
         public IEnumerable<SelectListItem> GetComboPropertyTypes()
         {
             var list = _dataContext.PropertyTypes.Select(pt => new SelectListItem
@@ -26,7 +46,8 @@ namespace MyLeasing.Web.Helpers
 
             list.Insert(0, new SelectListItem
             {
-                Text = "(Select a property type...)"
+                Text = "(Select a property type...)",
+                Value = "0"
             });
 
             return list;
